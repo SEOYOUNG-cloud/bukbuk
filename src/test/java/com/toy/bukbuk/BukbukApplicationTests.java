a358f82ca0c8aa1d9dc877cc098f61213eb248b6
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2025-06-03 15:44:01 +0900

    INIT: project init

---
+++ src/test/java/com/toy/bukbuk/BukbukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.bukbuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BukbukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
