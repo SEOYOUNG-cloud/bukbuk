2d8b05d7bff1f21e6a77375d69d0fdc03e39ec01
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2026-01-08 00:04:58 +0900

    FEAT: register @Bean, add JWT authentication filter to security filter chain

--- src/main/java/com/toy/bukbuk/auth/SecurityConfig.java
+++ src/main/java/com/toy/bukbuk/auth/SecurityConfig.java
@@ -1,14 +1,39 @@
 package com.toy.bukbuk.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Component
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor // final 필드 생성자 자동 생성(lombok)
 public class SecurityConfig {
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean // AuthenticationManager를 빈으로 등록
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // 서버에 인증정보 저장을 하지 않으므로
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/test").permitAll() // requestMatchers: 특정 url 패턴 지정
+                        .anyRequest().authenticated()) // 그 외
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 
 
     @Bean
