40d3302da488f329fd92d77bd7a31bb00331ce67
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2026-01-08 00:03:27 +0900

    REFACTOR: Split the responsibilities into methods - Requested By SonarQube

--- src/main/java/com/toy/bukbuk/auth/JwtRequestFilter.java
+++ src/main/java/com/toy/bukbuk/auth/JwtRequestFilter.java
@@ -27,31 +27,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
 
         try{
-            if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                String token = authHeader.substring(7);
-
-                // 토큰 유효성 검사
-                if(jwtUtil.isTokenValid(token)) {
-                    String username = jwtUtil.extractUsername(token);
-
-                    // jwt 검증, SpringContext에 인증 정보 세팅
-                    if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-
-                        // 토큰, 사용자 정보 비교검증
-                        if(jwtUtil.validateToken(token, userDetails)) {
-                            // SpringContext에 Authentication 정보 세팅
-                            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                            SecurityContextHolder.getContext().setAuthentication(authToken);
-                        }
-                    }
-                }
-
-            }
+            // 토큰 유효성 검사
+            authenticateIfPossible(request);
         } catch (UsernameNotFoundException e) {
             log.warn("User not found: {}", e.getMessage());
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -67,5 +46,55 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void authenticateIfPossible(HttpServletRequest request){
+        String token = extractToken(request);
+
+        if(token == null || !jwtUtil.isTokenValid(token)) {
+            return;
+        }
+
+        String username = jwtUtil.extractUsername(token);
+        if(username == null || isAlreadyAuthenticated()){
+            return;
+        }
+
+        // 사용자 정보 로드
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+        // 토큰 정보와 일치하는지 확인
+        if(!jwtUtil.validateToken(token, userDetails)) {
+            return;
+        }
+
+        // Setting Information in SecurityContext
+        setAuthentication(request, userDetails);
+    }
+
+    // 헤더에서 토큰 추출
+    private String extractToken(HttpServletRequest request){
+        String token = request.getHeader("Authorization");
+
+        if(token != null && token.startsWith("Bearer ")) {
+            return null;
+        }
+        return token;
+    }
+
+    private boolean isAlreadyAuthenticated(){
+        // SecurityContext에 인증 정보가 있는지 확인
+        return SecurityContextHolder.getContext().getAuthentication() != null;
+    }
+
+    private void setAuthentication(HttpServletRequest request, UserDetails userDetails) {
+        // Make authentication object in Spring Security
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        // 요청정보(IP, session ..) 세팅 - client IP 검사 시 사용
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        // Security Context에 인증정보 저장
+        SecurityContextHolder.getContext().setAuthentication(authToken);
     }
 }
