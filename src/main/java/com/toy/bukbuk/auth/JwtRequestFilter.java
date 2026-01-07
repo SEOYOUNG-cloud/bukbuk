2cb765c4d0e5ad6f6aca380ff9a3458eba679472
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2026-01-07 23:42:05 +0900

    FEAT: JwtFilter Component Setting

--- src/main/java/com/toy/bukbuk/auth/JwtRequestFilter.java
+++ src/main/java/com/toy/bukbuk/auth/JwtRequestFilter.java
@@ -11,13 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 @AllArgsConstructor
 @Slf4j
 public class JwtRequestFilter extends OncePerRequestFilter {
