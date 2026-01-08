d309fca9e1185182c2d419b96b7dbdbe9ad6019b
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2026-01-08 23:56:22 +0900

    FEAT: change Name

--- src/main/java/com/toy/bukbuk/auth/SignupRequestDto.java
+++ src/main/java/com/toy/bukbuk/auth/SignupRequestDto.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class LoginRequestDto {
+public class SignupRequestDto {
     private String email;
     private String password;
 }
