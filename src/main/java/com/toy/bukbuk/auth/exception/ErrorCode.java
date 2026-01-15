9f00c39c899a5d997ec11720c019e1015ba1c78a
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2026-01-16 00:39:20 +0900

    FEAT: add error code

--- src/main/java/com/toy/bukbuk/auth/exception/ErrorCode.java
+++ src/main/java/com/toy/bukbuk/auth/exception/ErrorCode.java
@@ -12,6 +12,12 @@ public enum ErrorCode {
             HttpStatus.CONFLICT,
             "EMAIL_DUPLICATED",
             "이미 존재하는 이메일입니다."
+    ),
+
+    USER_NOT_FOUND(
+      HttpStatus.NOT_FOUND,
+      "USER_NOT_FOUND",
+      "존재하지 않는 유저입니다."
     );
 
     private final HttpStatus status;
