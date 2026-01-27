5eb10d3f990162a1dba8bfe736c31db4c711e7f6
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2026-01-28 00:47:27 +0900

    FEAT: add error code

--- src/main/java/com/toy/bukbuk/auth/exception/ErrorCode.java
+++ src/main/java/com/toy/bukbuk/auth/exception/ErrorCode.java
@@ -8,6 +8,18 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    LOGIN_FAILED(
+            HttpStatus.UNAUTHORIZED,
+            "LOGIN_FAILED",
+            "이메일 또는 비밀번호가 올바르지 않습니다."
+    ),
+
+    INVALID_TOKEN(
+            HttpStatus.UNAUTHORIZED,
+            "INVALID_TOKEN",
+            "유효하지 않은 토큰입니다."
+    ),
+
     EMAIL_DUPLICATED(
             HttpStatus.CONFLICT,
             "EMAIL_DUPLICATED",
@@ -18,6 +30,18 @@ public enum ErrorCode {
       HttpStatus.UNAUTHORIZED,
       "USER_NOT_FOUND",
       "존재하지 않는 유저입니다."
+    ),
+
+    ACCESS_TOKEN_EXPIRED(
+        HttpStatus.UNAUTHORIZED,
+        "ACCESS_TOKEN_EXPIRED",
+        "Access Token이 만료되었습니다."
+    ),
+
+    REFRESH_TOKEN_EXPIRED(
+            HttpStatus.UNAUTHORIZED,
+            "REFRESH_TOKEN_EXPIRED",
+            "Refresh Token이 만료되었습니다."
     );
 
     private final HttpStatus status;
